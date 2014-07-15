91cf7e21e45a396251ed5a836888b40184ea2483
Daniel Gordon <dgordon@adobe.com>
2014-07-15 15:45:42 -0400

    boolean operations now using true false instead of 0 or 1

--- execute.ml
+++ execute.ml
@@ -24,18 +24,18 @@ let execute_prog prog =
     Lit i  -> stack.(sp) <- i; exec fp (sp+1) (pc+1)
   | Drp    -> exec fp (sp-1) (pc+1)
   | Bin op -> let op1 = stack.(sp-2) and op2 = stack.(sp-1) in     
-      stack.(sp-2) <- (let boolean i = if i then Int 1 else Int 0 in
+      stack.(sp-2) <- (
       match op with
 	Add     -> add op1 op2
       | Sub     -> subtract op1 op2
       | Mult    -> multiply op1 op2
       | Div     -> divide op1 op2
-      | Equal   -> boolean (to_Bool (equal op1 op2))
-      | Neq     -> boolean (to_Bool (nequal op1 op2))
-      | Less    -> boolean (to_Bool (lessthn op1 op2))
-      | Leq     -> boolean (to_Bool (lessthneq op1 op2))
-      | Greater -> boolean (to_Bool (greatthn op1 op2))
-      | Geq     -> boolean (to_Bool (greatthneq op1 op2))) ;
+      | Equal   -> equal op1 op2
+      | Neq     -> nequal op1 op2
+      | Less    -> lessthn op1 op2
+      | Leq     -> lessthneq op1 op2
+      | Greater -> greatthn op1 op2
+      | Geq     -> greatthneq op1 op2) ;
       exec fp (sp-1) (pc+1)
   | Lod i   -> stack.(sp)   <- globals.(i)  ; exec fp (sp+1) (pc+1)
   | Str i   -> globals.(i)  <- stack.(sp-1) ; exec fp sp     (pc+1)
@@ -46,8 +46,8 @@ let execute_prog prog =
   | Ent i   -> stack.(sp)   <- Int fp           ; exec sp (sp+i+1) (pc+1)
   | Rts i   -> let new_fp = cast_int stack.(fp) and new_pc = cast_int stack.(fp-1) in
                stack.(fp-i-1) <- stack.(sp-1) ; exec new_fp (fp-i) new_pc
-  | Beq i   -> exec fp (sp-1) (pc + if to_Bool(equal stack.(sp-1) (Int 0)) then i else 1)
-  | Bne i   -> exec fp (sp-1) (pc + if to_Bool(nequal stack.(sp-1) (Int 0)) then i else 1)
+  | Beq i   -> exec fp (sp-1) (pc + if to_Bool(equal stack.(sp-1) (Boolean false)) then i else 1)
+  | Bne i   -> exec fp (sp-1) (pc + if to_Bool(nequal stack.(sp-1) (Boolean false)) then i else 1)
   | Bra i   -> exec fp sp (pc+i)
   | Hlt     -> ()
 
